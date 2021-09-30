62842ae654b0af6d41534f17b9f083ffa4cf55b9
Natalia Petyaeva <npetyaeva@gmail.com>
2021-09-30 21:43:41 +0300

    Added Main.java & README.md

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int priceTicket = 9999;
+        int rublesPerMile = 20;
+
+        int countMiles = priceTicket / rublesPerMile;
+
+        System.out.println(countMiles + " miles");
+    }
+}
